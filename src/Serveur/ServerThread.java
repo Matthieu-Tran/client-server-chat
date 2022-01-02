96ecd121001c7dd39be93e6aee59cee374948f72
Matthieu <matthieu.tran@universite-paris-saclay.fr>
2022-01-02 17:54:19 +0100

    correction de quelques problemes

--- src/Serveur/ServerThread.java
+++ src/Serveur/ServerThread.java
@@ -29,12 +29,12 @@ public class ServerThread extends Thread {
                 String outputString = reader.readLine();
                 // On separe le pseudo et le message du client
                 String [] msg = outputString.split(": ", 2);
-                // Si l'utilisateur écrit "exit" ou bien envoie un message déja envoyé, il sera déconnecté
+                // Si l'utilisateur ecrit "exit" ou bien envoie un message deja envoye, il sera deconnecte
                 synchronized (lock){
                     if(Main.listeMessages.contains(msg[1]) || msg[1].contains("exit")) {
                         System.out.println(msg[0] + " a ete deconnecte :(");
                         if (Main.listeMessages.contains(msg[1]))
-                            output.println("Votre message a déja été envoyé, vous allez être déconnecté du serveur");
+                            output.println("Votre message a deja ete envoye, vous allez etre deconnecte du serveur");
                         else
                             output.println("Robot9000 vous dit au revoir");
                         //On envoie un message qui quand le client va le recevoir va etre deconnecte
